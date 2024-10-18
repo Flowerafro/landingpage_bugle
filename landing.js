c072b2156fb728a94d76cb8085a33599fb82e7aa
Flowerafro <linehenriksen0@gmail.com>
2024-10-18 10:13:32 +0200

    Test-landingpage
    testing

---
+++ landing.js
@@ -0,0 +1,22 @@
+let currentImageIndex = 0;
+const images = document.querySelectorAll('.gallery-image');
+
+function showImage(index) {
+    images.forEach((img, i) => {
+        img.style.display = i === index ? 'block' : 'none';
+    });
+}
+
+function nextImage() {
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    showImage(currentImageIndex);
+}
+
+function prevImage() {
+    currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+    showImage(currentImageIndex);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    showImage(currentImageIndex);
+});
